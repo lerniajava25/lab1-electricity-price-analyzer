06d235c923bbd07affb60a613565762bbda69b1c
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-08-24 14:55:12 +0200

    Dynamically set API URL based on current date and update data model timestamps to use `LocalDateTime`

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 //Datanhämtning med Javas inbyggd HttpClient
 public class Main {
@@ -14,10 +18,25 @@ public class Main {
                 .newBuilder()
                 .version(HttpClient.Version.HTTP_3)
                 .build();
+
+        // Anpassa URL:en till aktuellt datum istället för ett hårdkodat datum
+        LocalDate today = LocalDate.now(ZoneId.of("Europe/Stockholm"));
+
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("yyyy/MM-dd");
+
+        String date = today.format(formatter);
+
+        String url =
+                "https://www.elprisetjustnu.se/api/v1/prices/"
+                        + date
+                        + "_SE3.json";
         HttpRequest httpRequest = HttpRequest.newBuilder()
                 .GET()
-                .uri(URI.create("https://www.elprisetjustnu.se/api/v1/prices/2026/08-23_SE3.json"))
+                .uri(URI.create(url))
                 .build();
+
+
 
         var response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
         if (response.statusCode() != 200) {
@@ -28,12 +47,13 @@ public class Main {
         IO.println("HTTP GET: " + response.body());
 
 
+
         // Datamodell för ett elpris från api:et (objekt struktur beskrivning)
         record ElectricityPrice(double SEK_per_kWh,
                                 double EUR_per_kWh,
                                 double EXR,
-                                String time_start,
-                                String time_end) {
+                                LocalDateTime time_start,
+                                LocalDateTime time_end) {
 
         }
     }
