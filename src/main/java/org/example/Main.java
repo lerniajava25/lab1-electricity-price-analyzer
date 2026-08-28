6b3227465e333195bb61a7888023717e37f7caf8
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-08-28 18:22:44 +0200

    Add price area selection and refactor menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,96 +13,156 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 
-    public class Main {
+public class Main {
 
-        static void main() throws IOException, InterruptedException {
-            List<ElectricityPrice> prices = showApiIntegration();
-            ShowMenu(prices);
+    static void main() throws IOException, InterruptedException {
+        showMenu();
+    }
+
+    //1.1 API-Integration
+    // Datanhämtning med Javas inbyggd HttpClient
+    static List<ElectricityPrice> showApiIntegration(String selectedArea)
+            throws IOException, InterruptedException {
+
+        HttpClient httpClient = HttpClient
+                .newBuilder()
+                .connectTimeout(Duration.ofSeconds(10))
+                .version(HttpClient.Version.HTTP_3)
+                .build();
+
+        //Anpassa URL:en till aktuellt datum istället för ett hårdkodat datum
+        LocalDate today = LocalDate.now();
+
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("yyyy/MM-dd");
+
+        String date = today.format(formatter);
+
+        String url =
+                "https://www.elprisetjustnu.se/api/v1/prices/"
+                        + date
+                        + "_"
+                        + selectedArea
+                        + ".json";
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(url))
+                .timeout(Duration.ofSeconds(10))
+                .build();
+
+        var response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            IO.println("Fel vid hämtning av elpriser. HTTP-status: "
+                    + response.statusCode());
+            return new ArrayList<>();
         }
 
-        //1.1 API-Integration
-        // Datanhämtning med Javas inbyggd HttpClient
-        static List<ElectricityPrice> showApiIntegration()
-                throws IOException, InterruptedException {
-            HttpClient httpClient = HttpClient
-                    .newBuilder()
-                    .connectTimeout(Duration.ofSeconds(10))
-                    .version(HttpClient.Version.HTTP_3)
-                    .build();
+        //1.2 Create POJO from json => Konverterar JSON-svaret till en list av ElectricityPrice-objekt med Jackson
+        ObjectMapper mapper = new ObjectMapper();
 
-            //Anpassa URL:en till aktuellt datum istället för ett hårdkodat datum
-            LocalDate today = LocalDate.now(ZoneId.of("Europe/Stockholm"));
+        List<ElectricityPrice> prices = mapper.readValue(
+                response.body(),
+                new TypeReference<List<ElectricityPrice>>() {
+                }
+        );
+        return prices;
+    }
 
-            DateTimeFormatter formatter =
-                    DateTimeFormatter.ofPattern("yyyy/MM-dd");
-            String date = today.format(formatter);
-            String url =
-                    "https://www.elprisetjustnu.se/api/v1/prices/"
-                            + date
-                            + "_SE3.json";
-            HttpRequest httpRequest = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(URI.create(url))
-                    .timeout(Duration.ofSeconds(10))
-                    .build();
+    // 1. Visa hämtade elpriser (Case-1)
+    static void showPrices(List<ElectricityPrice> prices) {
+        for (ElectricityPrice price : prices) {
+            IO.println(price);
+        }
+    }
 
-            var response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() != 200) {
-                IO.println("Fel vid hämtning av elpriser. HTTP-status: "
-                        + response.statusCode());
-                return new ArrayList<>();
-            }
+    //Meny & Interaktivitet
+    static void showMenu() throws IOException, InterruptedException {
 
-            //1.2 Create POJO from json => Konverterar JSON-svaret till en list av ElectricityPrice-objekt med Jackson
-            ObjectMapper mapper = new ObjectMapper();
+        Scanner scanner = new Scanner(System.in);
 
-            List<ElectricityPrice> prices = mapper.readValue(
-                    response.body(),
-                    new TypeReference<List<ElectricityPrice>>() {
+        String selectedArea = null;
+        List<ElectricityPrice> prices = new ArrayList<>();
+
+        while (true) {
+            IO.println();
+            IO.println("Elpriser – Analysverktyg");
+            IO.println("========================");
+
+            showSelectedArea(selectedArea);
+
+            IO.println("1. Välj elområde (SE1, SE2, SE3, SE4)");
+            IO.println("2. Min, Max och Medelpris");
+            IO.println("3. Sortera priser (lägst till högst)");
+            IO.println("4. Bästa laddningstid (4h sammanhängande)");
+            IO.println("e. Avsluta");
+
+            String choice = scanner.nextLine();
+            switch (choice.toLowerCase()) {
+                case "1":
+                    IO.println("Välj elområde: SE1, SE2, SE3 eller SE4");
+                    String area = scanner.nextLine().toUpperCase();
+
+                    if (isValidArea(area)) {
+                        selectedArea = area;
+                        prices = showApiIntegration(selectedArea);
+
+                        IO.println("Elområde ändrat till " + selectedArea);
+                        showPrices(prices);
+                    } else {
+                        IO.println("Ogiltigt elområde.");
                     }
-            );
-            return prices;
-        }
+                    break;
 
-        // 1. Visa hämtade elpriser (Case-1)
-        static void showPrices(List<ElectricityPrice> prices) {
-            for (ElectricityPrice price : prices) {
-                IO.println(price);
+                case "2", "3", "4":
+                    handlePriceChoice(choice, prices);
+                    break;
+
+                case "e":
+                    IO.println("Programmet avslutas.");
+                    return;
+
+                default:
+                    IO.println("Ogiltigt val. Försök igen.");
             }
         }
+    }
 
-        //Meny & Interaktivitet
-        static void ShowMenu(List<ElectricityPrice> prices) {
-            String choice = "";
-            while (!choice.equalsIgnoreCase("e")) {
-                IO.println("Elpriser – Analysverktyg");
-                IO.println("1. Visa elpriser");
-                IO.println("2. Visa lägsta, högsta och medel pris");
-                IO.println("3. Sortera priser (lägst till högst)");
-                IO.println("4. Visa bästa laddningstid (4h sammanhängande)");
-                IO.println("E. Avsluta");
-
-                choice = IO.readln("Välj ett alternativ (1-4 eller E för att avsluta): ");
-
-                //Felhantering vid ongiltig inmatning
-                if (!choice.matches("[1-4eE]")) {
-                    IO.println("Ogiltigt alternativ! Försök igen.");
-                    continue;
-                }
-
-                switch (choice.toLowerCase()) {
-                    case "1" -> showPrices(prices);
-                    case "2" -> showPriceAnalysis(prices);
-                    case "3" -> sortedPrices(prices);
-                    case "4" -> showBestChargingTime(prices);
-                    case "e" -> IO.println("Programmet avslutas");
-                    default -> IO.println("Ogiltigt val");
-                }
-            }
+    static void showSelectedArea(String selectedArea) {
+        if (selectedArea == null) {
+            IO.println("Valt elområde: Inget område valt");
+        } else {
+            IO.println("Valt elområde: " + selectedArea);
         }
+    }
 
+    static boolean hasPrices(List<ElectricityPrice> prices) {
+        if (prices.isEmpty()) {
+            IO.println("Välj först ett elområde.");
+            return false;
+        }
+        return true;
+    }
+
+    static void handlePriceChoice(String choice, List<ElectricityPrice> prices) {
+        if (!hasPrices(prices)) {
+            return;
+        }
+        switch (choice) {
+            case "2" -> showPriceAnalysis(prices);
+            case "3" -> sortedPrices(prices);
+            case "4" -> showBestChargingTime(prices);
+            default -> IO.println("Ogiltigt val.");
+        }
+    }
+    static boolean isValidArea(String area) {
+        return area.equals("SE1")
+                || area.equals("SE2")
+                || area.equals("SE3")
+                || area.equals("SE4");
+    }
         //2. Elprisanalys (case-2)
         static void showPriceAnalysis(
                 List<ElectricityPrice> prices) {
@@ -135,8 +195,9 @@ import java.util.List;
         }
 
             //3. Sortera priser/case-3 (lägst till högst), genom att först skapa en kopia "sortedPrices"
-            static void sortedPrices (List<ElectricityPrice> prices) {
-                    List < ElectricityPrice > sortedPrices = new ArrayList<>(prices);
+
+         static void sortedPrices(List<ElectricityPrice> prices) {
+                    List< ElectricityPrice > sortedPrices = new ArrayList<>(prices);
             sortedPrices.sort(
                     Comparator.comparingDouble(ElectricityPrice::SEK_per_kWh)
             );
